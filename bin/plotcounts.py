f8ca7ae7dd18fbb1a645d24d3d8d376f05d91652
Amy Davis <adavis26@qub.ac.uk>
2024-10-07 14:25:18 +0100

    Plot frequency against rank on log-log axes

--- bin/plotcounts.py
+++ bin/plotcounts.py
@@ -1,4 +1,4 @@
-"""Plots word count distribution"""
+"""Plot word counts."""
 
 import argparse
 
@@ -6,17 +6,17 @@ import pandas as pd
 
 
 def main(args):
-    """Run the program."""
-    input_csv = args.infile
-    df = pd.read_csv(input_csv, header=None,
-                    names=('word', 'word_frequency'))
+    """Run the command line program."""
+    df = pd.read_csv(args.infile, header=None,
+                     names=('word', 'word_frequency'))
     df['rank'] = df['word_frequency'].rank(ascending=False,
-                                        method='max')
+                                           method='max')
     df['inverse_rank'] = 1 / df['rank']
     ax = df.plot.scatter(x='word_frequency',
-                            y='inverse_rank', loglog=True,
-                            figsize=[12, 6],
-                            grid=True, xlim = args.xlim)
+                         y='rank', loglog=True,
+                         figsize=[12, 6],
+                         grid=True,
+                         xlim=args.xlim)
     ax.figure.savefig(args.outfile)
 
 
@@ -24,13 +24,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('infile', type=argparse.FileType('r'),
                         nargs='?', default='-',
-                        help='Input file name')
-    parser.add_argument('--outfile', type=str, default='plotcounts.png',
-                        help='Output file name')
+                        help='Word count csv file name')
+    parser.add_argument('--outfile', type=str,
+                        default='plotcounts.png',
+                        help='Output image file name')
     parser.add_argument('--xlim', type=float, nargs=2,
                         metavar=('XMIN', 'XMAX'),
-                        default=None,
-                        help='x-axis bounds')
+                        default=None, help='X-axis limits')
     args = parser.parse_args()
     main(args)
     
